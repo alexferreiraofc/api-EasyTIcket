e653f21e9134f9cc4432963ed1a62c53987cafcb
alexferreiraofc <alex.ferreiraofc@gmail.com>
2023-05-11 23:53:43 -0300

    first commit

---
+++ src/main/java/com/easyticket/apieasyticket/ApiEasyticketApplication.java
@@ -0,0 +1,13 @@
+package com.easyticket.apieasyticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiEasyticketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiEasyticketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/easyticket/apieasyticket/ApiEasyticketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easyticket.apieasyticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiEasyticketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
